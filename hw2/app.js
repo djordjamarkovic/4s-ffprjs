0f21df3f20610f02e244c6e0042487c4ef60740e
Djordja Markovic <Djordja.Markovic@extern.wrwks.at>
2021-04-07 18:33:14 +0200

    scroll

--- hw2/app.js
+++ hw2/app.js
@@ -6,10 +6,44 @@
 // ********** set date ************
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
-
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
 
+navToggle.addEventListener('click', function() {
+    //linksContainer.classList.toggle("show-links");
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    //console.log(containerHeight);
+    const linksHeight = links.getBoundingClientRect().height;
+    //console.log(linksHeight);
+
+    if(containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`
+    } else {
+        linksContainer.style.height = 0;
+    }
+})
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
 // ********** fixed navbar ************
+window.addEventListener('scroll',function () {
+    //console.log(window.pageYOffset);
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+
+    if(scrollHeight > navHeight) {
+        navbar.classList.add('fix-nav');
+    } else {
+        navbar.classList.remove('fix-nav');
+    }
+
+    if(scrollHeight > navHeight + 200) {
+        topLink.classList.add('show-link');
+    } else {
+        topLink.classList.remove('show-link');
+    }
+});
 
 // ********** smooth scroll ************
 // select links
